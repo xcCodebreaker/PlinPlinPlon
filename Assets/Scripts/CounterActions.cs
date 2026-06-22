0635ad2561516de3db0b5ab314528be7e47d9d6e
xcCodebreaker <xccodebreaker619@gmail.com>
2026-06-22 23:37:50 +0600

    Changelog 0.2:  - Bug: Fixed CounterButtons UI not functioning properly  - CounterButtons now work via CounterActions script  - Added the final scene, work in progress

---
+++ Assets/Scripts/CounterActions.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class CounterActions : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private Button incrementButton;
+    [SerializeField] private Button decrementButton;
+
+    private int currentValue;
+
+    private void Start()
+    {
+        currentValue = 0;
+        UpdateScore();
+
+        incrementButton.onClick.AddListener(Increment);
+        decrementButton.onClick.AddListener(Decrement);
+    }
+
+    private void Increment()
+    {
+        currentValue++;
+        ScoreHandler();
+    }
+
+    private void Decrement()
+    {
+        currentValue--;
+        ScoreHandler();
+    }
+
+    private void ScoreHandler()
+    {
+        UpdateScore();
+
+        if (currentValue >= 10)
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+   
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = currentValue.ToString();
+    }
+}
